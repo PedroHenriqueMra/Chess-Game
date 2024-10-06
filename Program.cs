ad24c550bb409e7b5003a7eda6259908e0d1359c
PedroHenriqueMra <pedrohenriquemraa@gmail.com>
2024-10-06 15:36:39 -0300

    init project

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("XADREZ");
+
+/*
+    criar entidades e suas capacidades:
+        * peoes
+        * torres
+        * cavalos
+        * bispos
+        * rei
+        * rainha
+    matriz do tabuleiro:
+        * 8 espaço (vertical 1-8)
+        * 8 peças (horizontal a-h)
+    logica:
+        * turnos
+        * ganhar perder
+            * travar as peças
+            * acabar as peças
+            * o rei for tomado
+        * logica das peças
+        * peao no final
+    
+    classes:
+        * desenhar tabuleiro
+        * tratar o input do jogador
+        * peças
+            * proliferação
+        * logica e regras
+        * "IA" do oponente
+*/
+
+
